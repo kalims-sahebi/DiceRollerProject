f200e8d372db8afa0909121b7212023848929a52
kalim-sahebi <kalimapid@yahoo.com>
2024-11-03 23:16:54 +0430

    Custome Widget

--- lib/main.dart
+++ lib/main.dart
@@ -2,27 +2,36 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    MaterialApp(
+    const MaterialApp(
       home: Scaffold(
-          body: Container(
-        decoration: const BoxDecoration(
-          gradient: LinearGradient(
-            colors: [
-              Color.fromARGB(255, 255, 68, 81),
-              Color.fromARGB(255, 149, 43, 210),
-            ],
-            begin: Alignment.topLeft,
-            end: Alignment.bottomRight,
-          ),
-        ),
-        child: const Center(
-          child: Text(
-            'Hello World!',
-            style: TextStyle(fontSize: 50),
-          ),
-        ),
-      )),
+        body: GradientContainer(),
+      ),
     ),
   );
 }
-//testing git
+
+class GradientContainer extends StatelessWidget {
+  //const GradientContainer({key}) : super(key: key);
+  const GradientContainer({super.key});
+  @override
+  Widget build(context) {
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            Color.fromARGB(255, 255, 68, 81),
+            Color.fromARGB(255, 149, 43, 210),
+          ],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+        ),
+      ),
+      child: const Center(
+        child: Text(
+          'Hello World!',
+          style: TextStyle(fontSize: 50),
+        ),
+      ),
+    );
+  }
+}
